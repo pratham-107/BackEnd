380711f7e493fe174cb6e1b5cd397d03fd26cd24
pratham-107 <spratham845@gmail.com>
2025-02-04 22:54:14 +0530

    code update

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 function createUser(){
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let users = JSON.parse(localStorage.getItem("users"));
     let newUsers = {id: users.length + 1, name: "users" + (users.length + 1)};
     users.push(newUsers);
     localStorage.setItem("users", JSON.stringify(users));
-    console.log("User Create", newUsers);
+    console.log("User Created", newUsers);
 }
 
 function getUser(){
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let users = JSON.parse(localStorage.getItem("users"));
     console.log("All Users:", users);
 }
 
 function updateUser(){
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let users = JSON.parse(localStorage.getItem("users"));
     if (users.length > 0){
         users[0].name = "Updated User";
         localStorage.setItem("users", JSON.stringify(users));
-        console.log("First user Updated:", users[0]);
-    }
-    else{
+        console.log("User Updated", users[0]);
+    } else {
         console.log("No users to update");
     }
 }
-
 function deleteUser(){
     let users = JSON.parse(localStorage.getItem("users")) || [];
     if (users.length > 0){
         let removeUser = users.pop();
         localStorage.setItem("users", JSON.stringify(users));
-        console.log("User Deleted:", removeUser);
+        console.log("User Deleted", removeUser);
     }
     else{
         console.log("No users to delete");

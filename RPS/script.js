636aa939a6646b699864e98104b849bebba9a7d3
pratham-107 <spratham845@gmail.com>
2025-02-11 20:22:55 +0530

    backend code

---
+++ RPS/script.js
@@ -0,0 +1,44 @@
+function rock(){
+    let val = Math.floor(Math.random()*3);
+    let r = 0;
+    if(val == r){
+        alert("tie");
+    }
+    else if(val == 2){
+        alert("computer won");
+    }
+    else{
+        alert("you won");
+    }
+    console.log(val);
+}
+
+function paper(){
+    let val = Math.floor(Math.random()*3);
+    let p = 1;
+    if(val == p){
+        alert("tie");
+    }
+    else if(val == 0){
+        alert("computer won");
+    }
+    else{
+        alert("you won");
+    }
+    console.log(val);
+}
+
+function scissor(){
+    let val = Math.floor(Math.random()*3);
+    let s = 2;
+    if(val == s){
+        alert("tie");
+    }
+    else if(val == 1){
+        alert("computer won");
+    }
+    else{
+        alert("you won");
+    }
+    console.log(val);
+}
--- app.js
+++ app.js
@@ -50,3 +50,11 @@
 // console.log(jokes());
 
 
+// function fibo(n){
+//     let series =[0,1];
+//     for(let i =2; i<n; i++){
+//         series.push(series[i-1] + series[i-2]);
+//     }
+//     return series;
+// }
+// console.log(fibo(5));

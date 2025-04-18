6d6a7336a8b24f2bbd7c3845de5724839b26b862
pratham-107 <spratham845@gmail.com>
2025-04-18 21:07:21 +0530

    first commit

---
+++ chat.js
@@ -0,0 +1,29 @@
+// const ct = {
+//     stu1 : {
+//         name : "pratham",
+//         age : 21,
+//         gender: "male"
+//     },
+
+//     stu2 : {
+//         name : "singh",
+//         age : 20, 
+//         gender : "male"
+//     }
+// };
+
+// console.log(ct.stu1.name);
+
+
+
+const res = {
+
+    greet : {
+        hello: "hi",
+       "how r u" : "i am just a bot",
+    },
+    bye : "goodbye",
+    thanks : "your welcome"
+}
+
+console.log(res.greet["how r u"]);
